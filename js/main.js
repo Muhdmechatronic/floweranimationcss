755f1b33db52d8f4592fee25cd02abd50efdd5a6
Muhd99 <muhdismaail@gmail.com>
2024-10-09 08:50:22 +0800

    updatetext

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('Happy Birthday Hilmi 💓').split('')
+    const titles = ('Happy Birthday Siti Aisyah (Kaklong) 💓').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
